4ece41f029eb9606d3c931f4236c3eaa4818590b
daria-chr <cherkinadar@gmail.com>
2026-02-02 18:03:51 +0300

    Приветствие и ФИО

--- server/Program.cs
+++ server/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Здравствуйте. Добро пожаловать");
+string fio="Черкина Дарья Алексеевна";
+Console.WriteLine($"ФИО: {fio}");
