fd8a9f092c77f860df08d42eec36f6dba19e1dfd
daria-chr <cherkinadar@gmail.com>
2026-02-02 19:13:19 +0300

    добавление логики выбора

--- server/Program.cs
+++ server/Program.cs
@@ -5,10 +5,33 @@ string group="ИСП-231";
 Console.WriteLine($"ФИО: {fio}");
 Console.WriteLine($"Группа: {group}");
 Console.WriteLine($"Дата: {DateTime.Now}");
-Console.WriteLine("Меню:");
-Console.WriteLine("1 — Показать ФИО");
-Console.WriteLine("2 — Показать группу");
-Console.WriteLine("3 — Показать дату");
-Console.WriteLine("4 — Выход");
-
+int choice;
+do
+{
+    Console.WriteLine("Меню:");
+    Console.WriteLine("1 — Показать ФИО");
+    Console.WriteLine("2 — Показать группу");
+    Console.WriteLine("3 — Показать дату");
+    Console.WriteLine("4 — Выход");
+    Console.WriteLine("Выберите пункт меню: ");
+    choice = int.Parse(Console.ReadLine());
+    switch (choice)
+    {
+    case 1:
+    Console.WriteLine($"ФИО: {fio}");
+    break;
+    case 2:
+    Console.WriteLine($"Группа: {group}");
+    break;
+    case 3:
+    Console.WriteLine($"Дата: {DateTime.Now}");
+    break;
+    case 4:
+    Console.WriteLine("Программа завершает работу");
+    break;
+    default:
+    Console.WriteLine("Неверный выбор");
+    break;
+    }
+} while (choice != 4);
 
--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e13f5ff85fb2e97ff92cf906679b91b53ee3fb02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88b9cc6adf1cac6ecaab96d02904ca1c0437ff71")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
